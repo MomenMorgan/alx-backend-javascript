e24d07c4c76c8b7a84fcc8f26b4c947365f3e92a
MominMorgan <mominmorgan10@gmail.com>
2024-02-24 22:04:22 +0200

    building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = value;
+  }
+}
