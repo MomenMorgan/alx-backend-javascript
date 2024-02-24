f79c52aa07b7d665833b58475c19aa761852acd8
MominMorgan <mominmorgan10@gmail.com>
2024-02-24 16:08:59 +0200

    ANOTHER METHOD

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      console.log(
-        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`,
-      );
-    })
-    .catch(() => console.log('Signup system offline'));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
