d3aa75241202b75e5f49571fbea2ce55769931de
MominMorgan <mominmorgan10@gmail.com>
2024-02-24 21:21:25 +0200

    add setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCours {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    if (typeof this._name === 'string') {
+      return this._name;
+    }
+    return 'Error';
+  }
+
+  get length() {
+    if (typeof this._length === 'number') {
+      return this._length;
+    }
+    return 'Error';
+  }
+
+  get students() {
+    if (Array.isArray(this._students)) {
+      return this._students;
+    }
+    return 'Error';
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    }
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    }
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      this._students = newStudents;
+    }
+  }
+}
