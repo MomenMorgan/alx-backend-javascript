57493aaab5cd538c88dbe83ddc967dc89d2d8ed2
MominMorgan <mominmorgan10@gmail.com>
2024-02-27 14:11:55 +0200

    edit code

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,39 +1,38 @@
 const fs = require('fs');
 
-function countStudents(path) {
+const countStudents = (path) => {
+  let data;
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n');
-    const fields = lines.map((line) => line.split(','));
-    const cs = [];
-    const swe = [];
-    const coun = fields.length - 1;
-
-    for (let i = coun; i >= 0; i -= 1) {
-      if (fields[i].includes('CS')) {
-        cs.push(fields[i]);
-        fields.splice(i, 1);
-      } else if (fields[i].includes('SWE')) {
-        swe.push(fields[i]);
-        fields.splice(i, 1);
-      }
-    }
-    console.log(`Number of students: ${coun}`);
-    console.log(
-      `Number of students in CS: ${cs.length}. List: ${cs
-        .map((el) => el[0])
-        .reverse()
-        .join(', ')}`,
-    );
-    console.log(
-      `Number of students in SWE: ${swe.length}. List: ${swe
-        .map((el) => el[0])
-        .reverse()
-        .join(', ')}`,
-    );
-  } catch (error) {
+    data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
-}
+
+  data = data.split('\n');
+  let students = data.filter((student) => student);
+  students = students.map((item) => item.split(','));
+
+  const studentSize = students.length ? students.length - 1 : 0;
+  console.log(`Number of students: ${studentSize}`);
+
+  const fields = {};
+  for (const i in students) {
+    if (i !== 0) {
+      if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+      fields[students[i][3]].push(students[i][0]);
+    }
+  }
+
+  delete fields.field;
+
+  for (const key of Object.keys(fields)) {
+    console.log(
+      `Number of students in ${key}: ${fields[key].length}. List: ${fields[
+        key
+      ].join(', ')}`,
+    );
+  }
+};
 
 module.exports = countStudents;
