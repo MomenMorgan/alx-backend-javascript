03a3c5ccfca7e24fafa78972c99ea6395f657225
MominMorgan <mominmorgan10@gmail.com>
2024-03-02 13:56:01 +0200

    make a student table

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,43 @@
+export interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+//create tow students
+
+export const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 23,
+  location: "New York",
+};
+
+export const student2: Student = {
+  firstName: "Jane",
+  lastName: "Snow",
+  age: 25,
+  location: "Cairo",
+};
+
+const studentsList: Array<Student> = [student1, student2];
+
+function renderTable(students: Student[]) {
+  const table = document.getElementById("students-table");
+
+  studentsList.forEach((student) => {
+    const row = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = student.firstName;
+
+    const locationCell = document.createElement("td");
+    locationCell.textContent = student.location;
+
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+
+    table.appendChild(row);
+  });
+}
+
